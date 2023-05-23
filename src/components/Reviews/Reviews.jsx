1c669fe6e2ef618bf1b5b611e5a95bef3817b615
IhorProkopchuk <rectoon001@gmail.com>
2023-05-23 21:26:28 +0300

    Update Reviews.jsx

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -7,41 +7,46 @@ const Reviews = () => {
   const { movieId } = useParams();
   const [reviews, setReviews] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
 
   useEffect(() => {
     const getReviews = async () => {
       try {
         const { results } = await fetchMovieReviews(movieId);
         setReviews(results);
-        setLoading(false);
       } catch (error) {
-        console.log(error);
+        setError('Error fetching movie reviews. Please try again later.');
+      } finally { 
         setLoading(false);
       }
     };
+
     getReviews();
   }, [movieId]);
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
 
   return (
     <>
       <h2>Reviews</h2>
-      {loading ? (
-        <Loader />
-      ) : (
-        <>
-          {reviews.length > 0 ? (
-            <ul>
-              {reviews.map(({ author, content, id }) => (
-                <li key={id}>
-                  <h2>{author}</h2>
-                  <p>{content}</p>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <h2>We don't have any reviews for this movie</h2>
-          )}
-        </>
+      {reviews.length > 0
+        ? (<ul>
+          {reviews.map(({ author, content, id }) => (
+            <li key={id}>
+              <h2>{author}</h2>
+              <p>{content}</p>
+            </li>
+          ))}
+        </ul>)
+        : (
+        <h2>We don't have any reviews for this movie</h2>
       )}
     </>
   );
